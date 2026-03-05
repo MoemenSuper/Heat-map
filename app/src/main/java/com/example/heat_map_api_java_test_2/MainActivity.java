4cfe87752931b2187f032df9c8e2f6c7cfa1a6a5
YoussefKarray <youssefkarray606@gmail.com>
2026-03-05 17:09:19 +0100

    map

---
+++ app/src/main/java/com/example/heat_map_api_java_test_2/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.heat_map_api_java_test_2;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MainActivity";
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        Log.d(TAG, "onMapReady called");
+
+        enableMyLocation();
+        addHeatmap();
+
+        // Initial camera position (Sydney)
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10));
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void addHeatmap() {
+        List<LatLng> latLngList = new ArrayList<>();
+        latLngList.add(new LatLng(-33.852, 151.211));
+        latLngList.add(new LatLng(-33.852, 151.211));
+        latLngList.add(new LatLng(-33.853, 151.212));
+        latLngList.add(new LatLng(-33.854, 151.213));
+        latLngList.add(new LatLng(-33.855, 151.214));
+        latLngList.add(new LatLng(-33.856, 151.215));
+        latLngList.add(new LatLng(-33.857, 151.216));
+
+        HeatmapTileProvider provider = new HeatmapTileProvider.Builder()
+                .data(latLngList)
+                .build();
+
+        mMap.addTileOverlay(new TileOverlayOptions().tileProvider(provider));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableMyLocation();
+            } else {
+                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
